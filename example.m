cbd400821dee784b581f3f783749d6d83f75932f
MiguelSimao <masimao@sapo.pt>
2017-03-16 14:37:53 +0000

    updated example

--- example.m
+++ example.m
@@ -1,55 +1,47 @@
-
+% Script created to overcome the shortcomings of the later
+% (aGetDescriptors.m) code. It would only load samples with ".zip" files.
+% At this moment, we have all samples and they are all unzipped in
+% 'samplePath'.
+%
+% Coimbra, February 22, 2017
+% Miguel Simão
+%
 import ChalearnLAPSample.GestureSample
 import ChalearnLAPSample.Skeleton
 
-if isunix
-    samplePath = '/media/simao/Users/Simao/LocalDatasets/Chalearn';
-else
-    samplePath = 'D:\Users\Simao\LocalDatasets\Chalearn';
-end
 
-% if isempty(getenv('DRIVE_PATH'))
-%     error('You don''t have a link to the root folder. Please check your environment variables.'); end
+samplePath = 'D:\Users\Simao\LocalDatasets\Chalearn';
 
 % Samples can be found in the D structure:
-DIR = dir([samplePath filesep 'Sample*.zip']);
-fprintf('There are %i zipped samples to read.\n',length(DIR));
+D = dir([samplePath filesep 'Sample*']);
+D = D([D.isdir]);
 
-dirpath = @(X,i)[X(i).folder filesep X(i).name];
+N = length(D); % total number of samples
 
-s = [];
-sampleRange = 1:940;
-%sampleLim = 470;%470;
-%%
-% Extract sample data if needed:
-for i = 1:length(DIR)
-    fprintf('Extracting sample %i/%i...',i,sampleRange(end));
-    s = [s; GestureSample(feval(dirpath,DIR,i),0)];
-    fprintf(' Done.\n');
-end
-clear sampleLim dirpath samplePath
-
-
-%%
-% Construct FeatureExtractor objects:
-fprintf('Constructing extraction objects...\n');
-FEN = FeatureExtractor.SkeletonFE(s);
-
-fprintf('DONE!\n');
-
-% Apply FE functions:
-fprintf('Applying feature extraction functions. This will take a few minutes.\n');
-
-for i=1:length(s)
-    fprintf('Sample %i/%i...',i,length(s));
-    apply(FEN(i));
-    fprintf(' Done.\n');
-end
-
-% Getting descriptors:
-P = cell(length(FEN),1);
+P = cell(N,1);
 Q = P;
-for i=1:length(FEN)
-    P{i} = vertcat(FEN(i).getDescriptors.positions);
-    Q{i} = FEN(i).sampleObj.getTargets;
+t = tic;
+%% loop
+for i=1:N
+   
+    fprintf('Loading sample %i/%i ... ',i,N);
+    
+    % Build sample path:
+    S = [D(i).folder filesep D(i).name];
+    % Load GestureSample object:
+    S = GestureSample(S,0);
+    
+    % Construct FeatureExtractor.SkeletonFE object:
+    S = FeatureExtractor.SkeletonFE(S);
+    S.extractionFcns =   {'doNothing'};
+    S.selectedOutputNodes = 1:20;
+    
+    % Apply preprocessing steps to FE class:
+    apply(S);
+    % Get descriptors:
+    P{i} = cat(1,S.descriptors.positions);
+    Q{i} = S.sampleObj.getTargets;
+
+    seconds = toc(t)/i * (N-i);
+    fprintf('Done. Time remaining: %i:%is\n',floor(seconds/60),floor(rem(seconds,60)));
 end
